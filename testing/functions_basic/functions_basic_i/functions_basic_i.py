e765e9b7bf0ca7c490584ee6ae31a1352f9be6a7
Nathan Foster <nathancfoster.03@gmail.com>
2021-08-30 16:03:54 -0700

    intermediate funkys

--- testing/functions_basic/functions_basic_i/functions_basic_i.py
+++ testing/functions_basic/functions_basic_i/functions_basic_i.py
@@ -7,7 +7,7 @@ print(number_of_food_groups())
 #2
 def number_of_military_branches():
     return 5
-print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
 # Most likely undefineed, or maybe undefined 5... undefined
 
 #3
---
+++ testing/functions_basic/interFunky.py
@@ -0,0 +1,87 @@
+from typing import final
+
+
+x = [[5, 2, 3], [10, 8, 9]]
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'}
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [{'x': 10, 'y': 20}]
+#Update Values 1
+x[1][0] = 15
+print(x)
+students[0]['last_name'] = 'Bryant'
+print(students)
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+z[0]['y'] = 30
+print(z)
+
+#2 Iterate through dictionaries
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+def iterateDictionary(dict):
+    for x in dict:
+        for key, value in x.items():
+            print(key, '-', value)
+
+iterateDictionary(students)
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+#3 Get a value!
+def iterateDictionary(key_name, some_list):
+    for x in some_list:
+        for key, value in x.items():
+            if(key_name == key):
+                print(value)
+
+iterateDictionary('first_name', students)
+
+#4 Iterate throught a dictionary with list values
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key, value in some_dict.items():
+        print(len(value), key)
+        for x in value:
+            print(x)
+
+printInfo(dojo)
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
