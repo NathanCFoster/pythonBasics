c11f1bd86cd89580a5fbee46759386cc0303b608
Nathan Foster <nathancfoster.03@gmail.com>
2021-08-30 18:21:07 -0700

    chains

--- testing/functions_basic/basicClasses/classBank.py
+++ testing/functions_basic/basicClasses/classBank.py
@@ -4,31 +4,23 @@ class User:
         self.balance = initDeposit
     def make_deposit(self, amount):
         self.balance += amount
+        return self
     def make_withdrawl(self, amount):
         self.balance -= amount
+        return self
     def display_user_balance(self):
         print("User: %s\nBalance: $%d\n" %(self.name, self.balance))
+        return self
     def transfer_money(self, otherUser, amount, reason):
         self.balance -= amount
         otherUser.balance += amount
         print("Transfer of $%d for %s successful!\nNew Balance: $%d" %(amount, reason, self.balance))
+        return self
 Nathan = User("Nathan", 50)
 Bob = User("Bob", 10)
 unknown = User('none', 0)
-Nathan.make_deposit(50)
-Nathan.make_deposit(50)
-Nathan.make_deposit(50)
-Nathan.make_withdrawl(100)
-Nathan.display_user_balance()
-Bob.make_deposit(50)
-Bob.make_deposit(30)
-Bob.make_withdrawl(10)
-Bob.make_withdrawl(70)
-Bob.display_user_balance()
-unknown.make_deposit(10000)
-unknown.make_withdrawl(1000)
-unknown.make_withdrawl(40)
-unknown.make_withdrawl(9000)
-unknown.display_user_balance()
+Nathan.make_deposit(50).make_deposit(30).make_deposit(50).make_withdrawl(100).display_user_balance()
+Bob.make_deposit(50).make_deposit(30).make_withdrawl(10).make_withdrawl(70).display_user_balance()
+unknown.make_deposit(10000).make_withdrawl(1000).make_withdrawl(40).make_withdrawl(9000).display_user_balance()
 #bonus
 Nathan.transfer_money(unknown,50,"gas")
