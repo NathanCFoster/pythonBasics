0c92f4015ab42a4f2bf0c4e07095609cdef61b68
Nathan Foster <nathancfoster.03@gmail.com>
2021-09-02 15:17:31 -0700

    checkers make me dieeeeeeee

---
+++ algos/w3d4.js
@@ -0,0 +1,47 @@
+/* 
+  String: Is Palindrome
+  Create a function that returns a boolean whether the string is a strict palindrome. 
+    - palindrome = string that is same forwards and backwards
+  
+  Do not ignore spaces, punctuation and capitalization
+ */
+
+const str1 = "a x a";
+const expected1 = true;
+
+const str2 = "racecar";
+const expected2 = true;
+
+const str3 = "Dud";
+const expected3 = false;
+
+const str4 = "oho!";
+const expected4 = false;
+
+/**
+ * Determines if the given str is a palindrome (same forwards and backwards).
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {boolean} Whether the given str is a palindrome or not.
+ */
+function isPalindrome(str) { 
+    var expected = false;
+    for (let index = 0; index < str.length; index++) {
+        for (let x = str.length - index - 1; x >= 0; x++) {
+            if (str[index] == str[x]) {
+                expected = true;
+                break
+            } else {
+                expected = false;
+                break
+            }
+            
+        }
+    }
+    return expected
+}
+console.log(isPalindrome(str1));
+console.log(isPalindrome(str2));
+console.log(isPalindrome(str3));
+console.log(isPalindrome(str4));
--- flask/fundamentals/templateBasics/checkerboard/server.py
+++ flask/fundamentals/templateBasics/checkerboard/server.py
@@ -4,7 +4,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def checkers():
-    return render_template("index.html")
+    return render_template("index.html", num=10, size=480, sizeh=500, sizew=500, num1=8, odd=False, color1="black", color2="red")
+@app.route("/<num>")
+def numCheckers(num):
+    return render_template("index.html", num=int(num), sizeh=int(num)*50, sizew=400, num1=8, odd=False, color1="black", color2="red")
+@app.route('/<num1>/<num>')
+def numCheckers2(num1, num):
+    if(int(num) % 2 == 1 and int(num1) % 2 == 1):
+        return render_template("index.html", num=int(num), sizeh=int(num)*50, sizew=int(num1)*50, num1=int(num1), odd=True, color1="black", color2="red")
+    elif(int(num1) % 2 == 1 and int(num) % 2 == 0):
+        return render_template("index.html", num=int(num), sizeh=int(num)*50, sizew=int(num1)*50, num1=int(num1), odd=True, color1="black", color2="red")
+    else:
+        return render_template("index.html", num=int(num), sizeh=int(num)*50, sizew=int(num1)*50, num1=int(num1), odd=False, color1="black", color2="red")
+@app.route('/<num1>/<num>/<color1>/<color2>')
+def numCheckersColors(num1, num, color1, color2):
+    if(int(num) % 2 == 1 and int(num1) % 2 == 1):
+        return render_template("index.html", num=int(num), sizeh=int(num)*50, sizew=int(num1)*50, num1=int(num1), odd=True, color1=color1, color2=color2)
+    elif(int(num1) % 2 == 1 and int(num) % 2 == 0):
+        return render_template("index.html", num=int(num), sizeh=int(num)*50, sizew=int(num1)*50, num1=int(num1), odd=True, color1=color1, color2=color2)
+    else:
+        return render_template("index.html", num=int(num), sizeh=int(num)*50, sizew=int(num1)*50, num1=int(num1), odd=False, color1=color1, color2=color2)
 
 if(__name__ == "__main__"):
     app.run(debug=True)
