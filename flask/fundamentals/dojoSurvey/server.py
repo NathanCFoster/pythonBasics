df673f0e32184fe84ee3ca29fd795eb7d9eceac0
Nathan Foster <nathancfoster.03@gmail.com>
2021-09-07 15:09:20 -0700

    le survey

--- flask/fundamentals/dojoSurvey/server.py
+++ flask/fundamentals/dojoSurvey/server.py
@@ -7,17 +7,19 @@ app.secret_key = "key"
 def root():
     return render_template('index.html')
 
-@app.route("/process/<name>/<loc>/<lang>/<comm>", methods=['post'])
-def submit(name, loc, lang, comm):
+@app.route("/process/<name>/<loc>/<lang>/<comm>/<gender>/<icecream>", methods=['post'])
+def submit(name, loc, lang, comm, gender, icecream):
     session['name'] = name
     session['loc'] = loc
     session['lang'] = lang
     session['comm'] = comm
+    session['gender'] = gender
+    session['icecream'] = icecream
     return redirect('/result')
 
 @app.route("/result")
 def show():
-    return render_template("results.html", name=session['name'], loc = session['loc'], lang = session['lang'], comm = session['comm'])
+    return render_template("results.html", name=session['name'], loc=session['loc'], lang=session['lang'], comm=session['comm'], gender=session['gender'], iceream=session['icecream'])
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask/fundamentals/dojoSurvey/static/survey.js
+++ flask/fundamentals/dojoSurvey/static/survey.js
@@ -3,5 +3,27 @@ function process(element) {
     var location = document.getElementById('location').value;
     var language = document.getElementById('language').value;
     var comment = document.getElementById('comment').value;
-    element.action = "/process/" + name + "/" + location + "/" + language + "/" + comment;
+    var gender = "";
+    var icecreams = [];
+    var icecream = '';
+    if (document.getElementById("gender1").checked == true) {
+        gender = "male";
+    } else if (document.getElementById("gender2").checked == true) {
+        gender = "female";
+    } else {
+        gender = "other"
+    }
+    if (document.getElementById("vanilla").checked == true) {
+        icecreams.push("vanilla"); 
+    } if (document.getElementById("chocolate").checked == true) {
+        icecreams.push("chocolate");
+    } if (document.getElementById("strawberry").checked == true) {
+        icecreams.push("strawberry");
+    } if (icecreams.length > 1) {
+        for (let index = 0; index < icecreams.length-1; index++) {
+            icecream += icecreams[index] + ", ";
+        }
+        icecream += icecreams[icecreams.length-1];
+    }
+    element.action = "/process/" + name + "/" + location + "/" + language + "/" + comment + "/" + gender + "/" + icecream;
 }
