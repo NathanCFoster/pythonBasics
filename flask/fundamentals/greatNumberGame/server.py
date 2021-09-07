f73d2193c6cdae56014f25231405845df39145e6
Nathan Foster <nathancfoster.03@gmail.com>
2021-09-07 13:51:54 -0700

    bug fixes

--- flask/fundamentals/greatNumberGame/server.py
+++ flask/fundamentals/greatNumberGame/server.py
@@ -6,30 +6,68 @@ app.secret_key = "key"
 
 @app.route('/')
 def indexish():
+    if "scores" not in session:
+        session['scores'] = []
     if "num" in session:
         print(session['num'])
-        return render_template("index.html", winOrLose=session['winOrLose'], text=session['text'])
+        print(session['scores'])
+        return render_template("index.html", winOrLose=session['winOrLose'], text=session['text'], leaderboard=session['scores'])
     else:
         session['num'] = random.randint(1, 100)
+        session['attempts'] = 0
         print(session['num'])
-        return render_template("index.html", winOrLose="", text="")
+        return render_template("index.html", winOrLose="", text="", leaderboard=session['scores'])
 
 @app.route('/guess/<num>', methods=['post'])
 def guessing(num):
     myNum = session['num']
-    if(int(num) == int(myNum)):
-        session['text'] = "You guessed right the number was %d!" % (myNum)
-        session['winOrLose'] = True;
-        session['num'] = random.randint(1,100)
+    session['attempts'] += 1
+    attempts = session['attempts']
+    if(attempts >= 5):
+        session['text'] = "You took to long to guess, you lose!\nThe number was %d..." %(myNum)
+        session['winOrLose'] = False
+        return redirect('/')
+    elif(int(num) == int(myNum)):
+        session['text'] = "You guessed right in %d attempts the number was %d!" % (attempts, myNum)
+        session['winOrLose'] = True
         return redirect('/')
     elif(int(num) < int(myNum)):
         session['text'] = "Your guess was to low!"
-        session['winOrLose'] = False;
+        session['winOrLose'] = False
         return redirect("/")
     elif(int(myNum) < int(num)):
         session['text'] = "Your guess was to high!"
-        session['winOrLose'] = False;
+        session['winOrLose'] = False
         return redirect("/")
+
+@app.route('/submit/<name>', methods=['POST'])
+def submit(name):
+    lb = {}
+    lb[name] = "Number: %d</br>Attempts: %d" % (session['num'], session['attempts'])
+    session['scores'].append(lb)
+    session.modified = True
+    print('-' * 20)
+    print(session['scores'])
+    return redirect('/leaderboard')
+
+@app.route("/leaderboard")
+def leaderboard():
+    print("!" * 20)
+    print(session['scores'])
+    scores = session['scores']
+
+    return render_template("leaderboard.html", scores = scores)
+
+@app.route("/reset")
+def reset():
+    session.pop('attempts')
+    session.pop('num')
+    return redirect('/')
+
+@app.route("/hardreset")
+def hardreset():
+    session.clear()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask/fundamentals/greatNumberGame/static/numbers.js
+++ flask/fundamentals/greatNumberGame/static/numbers.js
@@ -2,3 +2,9 @@ function guess(element) {
     var num = document.getElementById('number').value;
     element.action = "/guess/" + num;
 }
+
+function submitThis(element) {
+    var name = document.getElementById('user').value;
+    element.action = "/submit/" + name;
+    console.log('this user' + name);
+}
