d0b6b6ce722b909a57e1e57e545e10d5705671dc
Nathan Foster <nathancfoster.03@gmail.com>
2021-09-10 15:39:22 -0700

    modularized

--- mysql/users_cr/server.py
+++ mysql/users_cr/server.py
@@ -1,52 +1,5 @@
-from flask import Flask, request, render_template, redirect
-from users import User
-
-app = Flask(__name__)
-app.secret_key = "key"
-
-@app.route("/")
-def main():
-    return render_template("index.html")
-
-@app.route("/submit", methods=['POST'])
-def add_user():
-    data = {
-        'first_name' : request.form['first_name'],
-        'last_name' : request.form['last_name'],
-        'email' : request.form['email'],
-    }
-    new_user_id = User.add_user(data)
-    return redirect('/')
-
-@app.route("/all_users")
-def all_users():
-    all_users = User.get_all()
-    return render_template("read.html", all_users = all_users)
-
-@app.route("/profile/<id>")
-def showProfile(id):
-    currentUser = User.findUser(id)
-    return render_template("userprofile.html", currentUser = currentUser[0])
-
-@app.route("/edit/<id>")
-def editProfile(id):
-    currentUser = User.findUser(id)
-    return render_template("editprofile.html", currentUser=currentUser[0])
-
-@app.route("/update/<id>", methods=['post'])
-def updatedProfile(id):
-    data = {
-        'first_name': request.form['first_name'],
-        'last_name': request.form['last_name'],
-        'email': request.form['email'],
-    }
-    User.updateUser(id, data)
-    return redirect("/profile/%s" %(id))
-
-@app.route("/delete/<id>")
-def deleteProfile(id):
-    User.deleteUser(id)
-    return redirect("/all_users")
+from user_app.controllers import users
+from user_app import app
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
---
+++ mysql/users_cr/user_app/controllers/users.py
@@ -0,0 +1,50 @@
+from flask import request, render_template, redirect
+from user_app.models.users import User
+from user_app import app
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/submit", methods=['POST'])
+def add_user():
+    data = {
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email'],
+    }
+    new_user_id = User.add_user(data)
+    return redirect('/')
+
+@app.route("/all_users")
+def all_users():
+    all_users = User.get_all()
+    return render_template("read.html", all_users = all_users)
+
+@app.route("/profile/<id>")
+def showProfile(id):
+    currentUser = User.findUser(id)
+    return render_template("userprofile.html", currentUser = currentUser[0])
+
+@app.route("/edit/<id>")
+def editProfile(id):
+    currentUser = User.findUser(id)
+    return render_template("editprofile.html", currentUser=currentUser[0])
+
+@app.route("/update/<id>", methods=['post'])
+def updatedProfile(id):
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+    }
+    User.updateUser(id, data)
+    return redirect("/profile/%s" %(id))
+
+@app.route("/delete/<id>")
+def deleteProfile(id):
+    User.deleteUser(id)
+    return redirect("/all_users")
+
+if __name__ == "__main__":
+    app.run(debug=True) 
